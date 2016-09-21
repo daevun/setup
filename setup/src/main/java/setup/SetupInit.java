30b18f74f50f225d9d345e858663fe5b727ec3c6
Daniel <Daniel@DESKTOP-H61QEVL>
2016-09-21 21:48:13 +0200

    Initial Commit

---
+++ setup/src/main/java/setup/SetupInit.java
@@ -0,0 +1,9 @@
+package setup;
+
+public class SetupInit {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo");
+	}
+	
+}
---
+++ setup/src/test/java/setup/SetupInitTest.java
@@ -0,0 +1,15 @@
+package setup;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SetupInitTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
